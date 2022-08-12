ff348cedd8dcfa2135ab5f33fa0df839451fe76d
Audionoob <98852220+Audionoob@users.noreply.github.com>
2022-08-12 20:38:24 +0200

    Add files via upload

---
+++ EjercicioTema4ParteA.java
@@ -0,0 +1,22 @@
+public class MyClass {
+    public static void main(String args[]) {
+        
+    // Declaramos la variable numeroif    
+     int numeroif = 3;
+     
+     // Declaramos los distintos casos posibles (a>0 , a=0 , a<0)
+     
+     if (numeroif > 0) {
+         System.out.println ("El número es positivo");
+     }
+     
+     if (numeroif == 0) {
+         System.out.println ("El número es igual a 0");
+     } 
+     
+     if (numeroif < 0) {
+         System.out.println ("El número es negativo"); 
+     }
+     
+    }
+}
