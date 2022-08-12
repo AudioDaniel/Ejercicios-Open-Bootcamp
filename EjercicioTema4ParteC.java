8c99c21a73f62cf5c2c1d0c4c447d7f84f2da393
Audionoob <98852220+Audionoob@users.noreply.github.com>
2022-08-12 21:00:18 +0200

    Add files via upload

---
+++ EjercicioTema4ParteC.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    
+    public static void main(String args[]) {
+      int numeroWhile = 3;
+      do{
+         numeroWhile++;
+         System.out.println(numeroWhile);   
+      } 
+    while (numeroWhile < 4);
+
+    }
+}
