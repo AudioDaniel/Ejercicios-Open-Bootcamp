df8d9b73030b3c3b76653f75cd963fca6cfdc6ea
Audionoob <98852220+Audionoob@users.noreply.github.com>
2022-08-25 14:14:42 +0200

    Add files via upload

---
+++ python/calculadoraedad.py
@@ -0,0 +1,13 @@
+print("Vamos a comprobar si eres mayor de edad")
+print("  ")
+print("Introduce tu edad")
+edad = int(input())
+
+if edad < 18 :
+    print ("Eres menor de edad")
+else:
+    print ("Eres mayor de edad")
+
+
+# Esta linea es para que el cmd no se cierre automáticamente
+input()
