6c840f864a049cb3313af77d168416557931e5f0
Audionoob <98852220+Audionoob@users.noreply.github.com>
2022-08-26 13:07:47 +0200

    Add files via upload

---
+++ python/listainversa.py
@@ -0,0 +1,10 @@
+lista = []
+
+#Añade cada número del 0 al 100 a la lista
+#Después invierte el orden de la lista con el metodo reverse()
+
+for x in range(0,101):
+    lista.append(x)
+lista.reverse()
+
+print(lista)
